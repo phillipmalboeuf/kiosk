247be6bc72855f3c12d8c7d8784a552b0a09c839
Phillip Malboeuf <phil@phils.computer>
2023-04-23 15:14:58 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,62 @@
+<script lang='ts'>
+	import '../theme.postcss';
+	// import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+
+	import '../app.postcss';
+	import { AppShell, AppBar, AppRail, AppRailTile, ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+
+	let valueSingle: string = 'books';
+</script>
+
+<!-- App Shell -->
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl">Kioskberry</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<!-- <a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a> -->
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	
+	<svelte:fragment slot="sidebarLeft">
+		<!-- Hidden below Tailwind's large breakpoint -->
+		<div id="sidebar-left" class="hidden lg:block">
+			<ListBox>
+	<ListBoxItem bind:group={valueSingle} name="medium" value="books">Books</ListBoxItem>
+	<ListBoxItem bind:group={valueSingle} name="medium" value="movies">Movies</ListBoxItem>
+	<ListBoxItem bind:group={valueSingle} name="medium" value="tv">TV</ListBoxItem>
+</ListBox>
+
+		</div>
+	</svelte:fragment>
+
+	<slot />
+</AppShell>
